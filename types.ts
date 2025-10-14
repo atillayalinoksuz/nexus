20467c315c5b0ea2c63dbb15b18a4c0b8b50a2d3
atillayalinoksuz <atillayalinoksuzgithub@gmail.com>
2025-10-14 22:20:31 +0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,7 +1,13 @@
+export interface SubLink {
+  title: string;
+  url: string;
+}
+
 export interface SearchResult {
   id: number;
   title: string;
   url: string;
   description: string;
   category: string;
+  subLinks?: SubLink[];
 }
